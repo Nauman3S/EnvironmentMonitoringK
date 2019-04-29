eeb2f9ca4729279dd934b1a460b90913d93d1f1b
NaumanShakir <naumanshakir3s@gmail.com>
2019-04-29 16:00:59 +0500

    remove unnecessary code
    Removed should componentupdate function from gauge element

--- awesome-project/src/GaugeElement.js
+++ awesome-project/src/GaugeElement.js
@@ -3,16 +3,7 @@ import CircularProgressbar from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export  class GaugeElement extends React.Component {
-    shouldComponentUpdate(nextProps) {
-        console.log("thisState",this.props.val);
-        console.log("nextState",this.nextProps.val);
-      if(this.props.val===this.nextProps.val){
-        return false;
-      }
-      if(this.props.val!==this.nextProps.val){
-        return true;
-      }
-    }
+  
     constructor(props) {
         super(props);
         this.state={val:this.props.val};
